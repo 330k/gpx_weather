8be3e72d46f7e3b83272973e33d32e752d7863c0
330k <kei.misawa@gmail.com>
2022-11-16 09:06:49 +0900

    modify icon transparency

--- icons/convert.sh
+++ icons/convert.sh
@@ -2,5 +2,5 @@
 
 for s in 192 240 256 400 512;
 do
-  convert -resize "${s}x${s}" icon_850x850.svg "icon_${s}x${s}.png"
+  convert -resize "${s}x${s}" -background transparent icon_850x850.svg "icon_${s}x${s}.png"
 done
